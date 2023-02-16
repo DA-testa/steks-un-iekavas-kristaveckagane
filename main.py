aa7fc582413bcae9ac0382b437bc31acb9a432fc
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-02-16 10:28:46 +0000

    test

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def find_mismatch(text):
 
         elif next in ")]}":
 
-            if len(opening_brackets_stack)==0 
+            if len(opening_brackets_stack)==0:
             or not are_matching(opening_brackets_stack[-1].char,next_char):
              return i+1                             
             opening_brackets_stack.pop()
