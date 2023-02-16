ae2958229ac8a8351c6e3585d7b9a84df935445c
kristaveckagane <124573561+kristaveckagane@users.noreply.github.com>
2023-02-16 10:02:45 +0000

    test

--- main.py
+++ main.py
@@ -11,21 +11,26 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            # Process opening bracket, write your code here
-            pass
+    for i, next_char in enumerate(text):
+        if next_char in "([{":
+            opening_brackets_stack.append(Bracket(next_char, i+1)
+            #pass
 
         if next in ")]}":
-            # Process closing bracket, write your code here
-            pass
+
+            if len(opening_brackets_stack)==0 
+            or not are_matching(opening_brackets_stack[-1].char,next_char):
+            return i+1                             
+            opening_brackets_stack.pop()
+            if len(opening_brackets_stack) > 0:
+            return opening_brackets_stack[0].position
+            else: 
+                return "Success"
+            #pass
 
 
 def main():
-    text = input()
+    ievade = input("Input-I, File-F")
+    if ievade.lower() == "f":                                                                           
+    text = input("")                                      
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-
-
-if __name__ == "__main__":
-    main()
